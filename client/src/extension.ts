1372173184d880db892908efc56e56e914d225d2
Caleb <caleblpena@gmail.com>
2024-03-14 22:24:26 -0600

    add file types

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -29,7 +29,10 @@ export function activate(context: ExtensionContext) {
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for all documents by default
-		documentSelector: [{ scheme: 'file', language: '*' }],
+		documentSelector: [
+			{ scheme: 'file', language: 'css' },
+			{ scheme: 'file', language: 'svelte' },
+		],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
